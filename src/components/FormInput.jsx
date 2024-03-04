b7fd48ebd1aa385779786c0880bf1e8f3f3188bd
kiksonn <kikson.yy@gmail.com>
2024-03-04 22:58:30 +0100

    created validation

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,21 +1,32 @@
 
-function FormInput({ input, setInput, text, textImported}) {
+function FormInput({ input, setInput, text, textImported }) {
+    let textArray = text.split("");
+    textArray = textArray.slice(1);
 
-  return (
-    <div className={!textImported && "hidden"}>
-        <div>
-            {/*   //3 Przekazanie propsow z TextGenerator do home a z home do FormInput */}
-            <h3>{text}</h3>
+    const formattedText = textArray.map((char, index) => {
+        let colorClass = ""; // Klasa koloru, która zostanie przypisana do elementu span
+        if (index < input.length) {
+            colorClass = char === input[index] ? "text-green-500" : "text-red-500"; // Kolory zielony i czerwony z Tailwind CSS
+        }
+        return (
+            <span key={`${char}_${index}`} className={colorClass}>{char}</span>
+        );
+    });
+
+    return (
+        <div className={!textImported && "hidden"}>
+            <div>
+                <h3>{formattedText}</h3>
+            </div>
+            <div>
+                <form>
+                    <div>
+                        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type here"/>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div>
-            <form>
-                <div>
-                    <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type here"/>
-                </div>
-            </form>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default FormInput;
